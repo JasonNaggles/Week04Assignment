4f5419c3e523520a4863b233ea85d03f6656ccdb
JasonNaggles <JasonNaggles.verizon.net>
2023-06-24 18:27:19 -0400

    Modifying index.html and Week04Jason.js

--- Week04Jason.js
+++ Week04Jason.js
@@ -1,4 +1,4 @@
-//Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
+1. //Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
 //Programmatically subtract the value of the first element in the array from the value in the last element of the array.
 //Do not use numbers to reference the last element, find it programmatically.
 //ages[7] - ages[0] is not allowed!
@@ -9,7 +9,7 @@ var ages = [3, 9, 23, 64, 2, 8, 28, 93];
 var lastIndex = ages.length - 1;
 var firstElement = ages[0];
 var lastElement = ages[lastIndex];
-var newAge = 55;
+var newAge = 90;
 
 var subtractResult = lastElement - firstElement;
 console.log("Subtract Result", subtractResult);
